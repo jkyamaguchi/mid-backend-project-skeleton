3d6fa232530ad0f5ef4a4b2391a359a1337fedea
Juliana <ju.yamaguchi@gmail.com>
2026-05-14 23:09:12 +0200

    feat: add orders router to API for handling order-related requests

--- api/src/routers/api.js
+++ api/src/routers/api.js
@@ -2,11 +2,13 @@ import express from "express";
 import eventsRouter from "#routers/events.js";
 import authRouter from "#routers/auth.js";
 import cartsRouter from "#routers/carts.js";
+import ordersRouter from "#routers/orders.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/events", eventsRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/cart", cartsRouter);
+apiRouter.use("/orders", ordersRouter);
 
 export default apiRouter;
