9241702c27bca1a5e71a6d59813b4debb42b47bc
Juliana <ju.yamaguchi@gmail.com>
2026-05-21 22:21:55 +0200

    feat: update Swagger configuration to use current host URL for server description

--- api/src/configs/swagger.js
+++ api/src/configs/swagger.js
@@ -20,8 +20,8 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT ?? 3000}`,
-        description: "Local development server",
+        url: "/",
+        description: "Current host",
       },
     ],
     tags: [
